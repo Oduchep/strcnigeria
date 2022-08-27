63b3c955b21c3e1c7b67e39eb70f06917c3cc26b
Peter Okerulu <oduchep@gmail.com>
2022-08-27 15:33:43 +0100

    added banner photos and about us page height

---
+++ pages/about-us.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Head from 'next/head';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <Head>
+        <title> Society of Traditional Roman Catholics, Nigeria</title>
+        <meta
+          name="description"
+          content="society of traditional roman catholics of nigeria"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="h-[100vh]">
+        <p className="mt-20 text-center">In progress</p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
