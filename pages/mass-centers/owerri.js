343001c9b2fe2a26bd11ba29280066f57827ab20
Peter Okerulu <oduchep@gmail.com>
2022-09-15 19:27:29 +0100

    added owerri mission page

--- pages/mass-centers/owerri.js
+++ pages/mass-centers/owerri.js
@@ -1,7 +1,203 @@
-import React from 'react';
+import Image from 'next/image';
 
 const Owerri = () => {
-  return <div>Owerri</div>;
+  return (
+    <>
+      <section className="bg-[url('https://res.cloudinary.com/strcnigeria/image/upload/v1663237576/Mass%20Centers/Bode%20Mission/IMG-20220903-WA0015_le6ja4.jpg')] h-80 md:h-[28rem] bg-cover bg-center bg-no-repeat relative">
+        <div className="opacity-70 w-full h-full absolute top-0 bg-primary"></div>
+        <div className="bg-gradient-to-l from-primary-2-light flex items-center justify-center md:justify-end h-full relative z-10">
+          <div className="p-5 md:p-40">
+            <h1 className="text-white mb-4 text-4xl md:text-6xl">
+              Owerri Mission
+            </h1>
+            {/* <h2 className="text-white text-xl">
+              St Jean-Baptiste-Marie Vianney Catholic Chapel
+            </h2> */}
+          </div>
+        </div>
+      </section>
+
+      <section className="main-container grid lg:grid-cols-3 gap-y-8 gap-x-32">
+        <div className="order-2 lg:order-1 col-span-3 lg:col-span-2">
+          <div>
+            <h3 className="capitalize text-center text-primary text-2xl md:text-4xl mb-8">
+              History of STRC Owerri Mission
+            </h3>
+            <div className="text-lg">
+              <p>
+                Owerri STRC, Nigeria was formed in the year 2005 by some
+                students of the Federal University of Technology, Owerri (FUTO)
+                and a student of the Federal Polytechnic¸Nekede who were
+                disheartened as a result of the numerous heresies, non-Catholic
+                practices associated with the Vatican 2 Church.
+              </p>
+              <br />
+              <p>
+                These men decided to leave the Novus Ordo after due consultation
+                (through the help of their leader) with Bishop D. Dolan who
+                later sent them a priest (Fr. Eldred Leslie) in the year 2006
+                who helped to give the group a solid foundation, thus
+                establishing a viable mission in Nigeria. The members were about
+                11 men at the beginning. However, after these students graduated
+                and some traveled to other parts of the country, especially
+                Lagos and Port Harcourt, they helped to establish missions in
+                these locations. Two members who remained in Owerri continued to
+                sustain the group in Owerri as soon as they were back from their
+                NYSC(Nigerian Youth Service Corps) programmes. Hence, by the
+                year 2010, STRC, Nigeria had three branches, namely, STRC
+                Owerri, STRC Port Harcourt and STRC Lagos, respectively.
+              </p>
+              <br />
+              <p>
+                The group in Owerri received about this time the membership of a
+                courageous lady who joined the group shortly after she was
+                convinced concerning the errors of Vatican 2. And after the
+                death of Father Leslie in 2011, the group through the
+                benevolence of Bishop Dolan was visited by Frs. Carlos Ercoli
+                and Julian Larabee before the ordination of Fr. Nkamuke in 2013.
+                The Owerri group which was later named St. Louis De Montfort
+                mission always met on Sundays for prayers and discussions on
+                matters of Faith and Morals in one of the classrooms within St.
+                Paul’s Primary School, off Douglas Road, Owerri. On Fridays of
+                Lent, we initially held our Stations of the Cross in this
+                Primary School for about two years, but later gathered (for the
+                sake of convenience) at either the Pastoral Centre or the
+                Carmelite Order both situated along Wetheral Road, Owerri for
+                both the Stations of the Cross and the Sacred Heart devotion.
+                The number of the members which was originally about four
+                persons around 2010 increased to about twelve in 2014. Besides,
+                we occasionally enjoyed visits by some brethren resident in Port
+                Harcourt and Lagos.
+              </p>
+              <br />
+              <p>
+                After Father Nkamuke's arrival in 2014, the Sunday Masses were
+                held in a members house in Nekede, whereas we continued our
+                Sunday meetings in St. Paul’s Primary School, Owerri, whenever
+                the priest was not around. However, when the members
+                progressively increased to a sizable number, the need to look
+                for a more spacious and convenient place to accommodate the
+                members arose. Consequently, in the year 2019, we rented an
+                apartment which serves as our present chapel and houses some few
+                young ladies answering the call to the religious vocation.
+              </p>
+            </div>
+          </div>
+
+          <div className="mt-10 grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div className="col-span-1 h-80">
+              <div className="relative h-full w-full">
+                <Image
+                  src="https://res.cloudinary.com/strcnigeria/image/upload/v1663237575/Mass%20Centers/Bode%20Mission/IMG-20220903-WA0016_hg4wre.jpg"
+                  layout="fill"
+                  objectFit="cover"
+                  priority="true"
+                  alt="image"
+                />
+              </div>
+            </div>
+
+            <div className="col-span-1 h-80">
+              <div className="relative h-full w-full">
+                <Image
+                  src="https://res.cloudinary.com/strcnigeria/image/upload/v1663237573/Mass%20Centers/Bode%20Mission/IMG-20220903-WA0014_ewvzmc.jpg"
+                  layout="fill"
+                  objectFit="cover"
+                  priority="true"
+                  alt="image"
+                />
+              </div>
+            </div>
+
+            <div className="col-span-1 h-80">
+              <div className="relative h-full w-full">
+                <Image
+                  src="https://res.cloudinary.com/strcnigeria/image/upload/v1663237573/Mass%20Centers/Bode%20Mission/IMG-20220903-WA0013_atmq32.jpg"
+                  layout="fill"
+                  objectFit="cover"
+                  priority="true"
+                  alt="image"
+                />
+              </div>
+            </div>
+
+            <div className="col-span-1 h-80">
+              <div className="relative h-full w-full">
+                <Image
+                  src="https://res.cloudinary.com/strcnigeria/image/upload/v1663237572/Mass%20Centers/Bode%20Mission/IMG-20220903-WA0012_hgazvc.jpg"
+                  layout="fill"
+                  objectFit="cover"
+                  priority="true"
+                  alt="image"
+                />
+              </div>
+            </div>
+            <div className="col-span-1 h-80">
+              <div className="relative h-full w-full">
+                <Image
+                  src="https://res.cloudinary.com/strcnigeria/image/upload/v1663237572/Mass%20Centers/Bode%20Mission/IMG-20220903-WA0010_p8yyxw.jpg"
+                  layout="fill"
+                  objectFit="cover"
+                  priority="true"
+                  alt="image"
+                />
+              </div>
+            </div>
+            <div className="col-span-1 h-80">
+              <div className="relative h-full w-full">
+                <Image
+                  src="https://res.cloudinary.com/strcnigeria/image/upload/v1663237575/Mass%20Centers/Bode%20Mission/IMG-20220903-WA0017_uilijt.jpg"
+                  layout="fill"
+                  objectFit="cover"
+                  priority="true"
+                  alt="image"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-3 lg:col-span-1 order-1 lg:order-2">
+          <div className="mb-10 shadow-lg shadow-primary-light/30">
+            <h3 className="uppercase text-center text-white text-lg p-3 bg-primary-lighter">
+              address
+            </h3>
+            <div className="p-5">
+              <p>
+                The chapel is located at № 67 Lobo Extension, off Wetheral Road
+                (Close to Emmanuel College), Umuoyima, Owerri, Imo State.
+              </p>
+            </div>
+          </div>
+
+          <div className="mb-10 shadow-lg shadow-primary-light/30">
+            <h3 className="uppercase text-center text-white text-lg p-3 bg-primary-lighter">
+              priests
+            </h3>
+            <div className="p-5">
+              <p>Reverend Father Nkamuke Bede (+234 08165672764)</p>
+              <p>Reverend Father Ojeka Thomas (+234 07049047920)</p>
+              <p>Reverend Father Okerulu John (+234 07064809668)</p>
+            </div>
+          </div>
+
+          <div className="mb-10 shadow-lg shadow-primary-light/30">
+            <h3 className="uppercase text-center text-white text-lg p-3 bg-primary-lighter">
+              current activities
+            </h3>
+            <div className="p-5">
+              <p>
+                The activities of the members take place on Saturdays (for altar
+                servers meeting) and on Sundays for Mass, prayers/discussion. We
+                are yet to get a good choir as members stay quiet at a distance
+                away from the chapel.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Owerri;
