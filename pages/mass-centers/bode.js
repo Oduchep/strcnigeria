f742fb7a7457f43a2e443145beaf41cf3df14341
Peter Okerulu <oduchep@gmail.com>
2022-09-15 12:12:28 +0100

    bode mission page

--- pages/mass-centers/bode.js
+++ pages/mass-centers/bode.js
@@ -1,7 +1,232 @@
-import React from 'react';
+import Image from 'next/image';
 
 const Bode = () => {
-  return <div>Bode</div>;
+  return (
+    <>
+      <section className="bg-[url('https://res.cloudinary.com/strcnigeria/image/upload/v1663237576/Mass%20Centers/Bode%20Mission/IMG-20220903-WA0015_le6ja4.jpg')] h-80 md:h-[28rem] bg-cover bg-center bg-no-repeat relative">
+        <div className="opacity-70 w-full h-full absolute top-0 bg-primary"></div>
+        <div className="bg-gradient-to-l from-primary-2-light flex items-center justify-center md:justify-end h-full relative z-10">
+          <div className="p-5 md:p-40">
+            <h1 className="text-white mb-4 text-4xl md:text-6xl">
+              Agenebode Mission
+            </h1>
+            <h2 className="text-white text-xl">
+              St Jean-Baptiste-Marie Vianney Catholic Chapel
+            </h2>
+          </div>
+        </div>
+      </section>
+
+      <section className="main-container grid lg:grid-cols-3 gap-16">
+        <div className="order-2 lg:order-1 col-span-3 lg:col-span-2">
+          <div>
+            <h3 className="capitalize text-center text-primary text-2xl md:text-4xl mb-8">
+              a brief history of St Jean-Baptiste-Marie Vianney Catholic Chapel
+            </h3>
+            <p className="text-lg">
+              In 2016 three young men having discovered the faith by the
+              instrumentality of a young devout Novus Ordo seminarian who worked
+              in that district for some couple of months, thought it right that
+              the novus ordo church was not a place, where one could receive
+              valid sacraments and fulfill the precepts of the church,
+              especially that of going to church on Sundays and holy days of
+              obligation, On account of these reasons, these three young men
+              deemed it fit to look for well trained valid traditional Catholic
+              priests who could administer to them valid sacraments.
+              <br />
+              <br />
+              On their quest for these priests, they stumbled upon many
+              traditional groups and traditional Catholics on the internet ,
+              i.e. on Facebook, Who were of great help to the realization of
+              getting a good well trained and uncompromising Catholic priest.
+              This plan and good resolution was actualized after much prayer and
+              diligent research by the finding of Reverend father Nkamuke Bede
+              Okechukwu. The finding of Rev. Fr. Nkamuke Bede brought great
+              enlightenment and joy to the minds of these young men who were
+              thirsting for the refreshing water of life that is the true
+              Catholic Faith and Sacrament, just as the Psalmist says, “For you
+              my soul is thirsting O Lord, my God. (Psalm 62).
+              <br />
+              <br />
+              According to the design of Providence, on October 24th 2016, (the
+              Feast of Saint Raphael the Archangel), these young men first
+              witnessed the beauty, excellence and grandeur of the sacred
+              Catholic Mass. As soon as they witnessed the Mass, their love for
+              the sacred truth of the Catholic faith was deepened and
+              strengthened.
+              <br />
+              <br />
+              To the Greater Glory of God, This mission (St John-Vianney) has
+              witnessed a great increase in the number of faithful Catholics who
+              have also seen the level of the decadence of Catholic truth and
+              teachings in the Novus Ordo Church. The mission at present is
+              located in a rented apartment.
+              <br />
+              <br />
+              St. John Mary-Vianney. Pray for us
+            </p>
+          </div>
+
+          <div className="mt-10 grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div className="col-span-1 h-80">
+              <div className="relative h-full w-full">
+                <Image
+                  src="https://res.cloudinary.com/strcnigeria/image/upload/v1663237575/Mass%20Centers/Bode%20Mission/IMG-20220903-WA0016_hg4wre.jpg"
+                  layout="fill"
+                  objectFit="cover"
+                  priority="true"
+                  alt="image"
+                />
+              </div>
+            </div>
+
+            <div className="col-span-1 h-80">
+              <div className="relative h-full w-full">
+                <Image
+                  src="https://res.cloudinary.com/strcnigeria/image/upload/v1663237573/Mass%20Centers/Bode%20Mission/IMG-20220903-WA0014_ewvzmc.jpg"
+                  layout="fill"
+                  objectFit="cover"
+                  priority="true"
+                  alt="image"
+                />
+              </div>
+            </div>
+
+            <div className="col-span-1 h-80">
+              <div className="relative h-full w-full">
+                <Image
+                  src="https://res.cloudinary.com/strcnigeria/image/upload/v1663237573/Mass%20Centers/Bode%20Mission/IMG-20220903-WA0013_atmq32.jpg"
+                  layout="fill"
+                  objectFit="cover"
+                  priority="true"
+                  alt="image"
+                />
+              </div>
+            </div>
+
+            <div className="col-span-1 h-80">
+              <div className="relative h-full w-full">
+                <Image
+                  src="https://res.cloudinary.com/strcnigeria/image/upload/v1663237572/Mass%20Centers/Bode%20Mission/IMG-20220903-WA0012_hgazvc.jpg"
+                  layout="fill"
+                  objectFit="cover"
+                  priority="true"
+                  alt="image"
+                />
+              </div>
+            </div>
+            <div className="col-span-1 h-80">
+              <div className="relative h-full w-full">
+                <Image
+                  src="https://res.cloudinary.com/strcnigeria/image/upload/v1663237572/Mass%20Centers/Bode%20Mission/IMG-20220903-WA0010_p8yyxw.jpg"
+                  layout="fill"
+                  objectFit="cover"
+                  priority="true"
+                  alt="image"
+                />
+              </div>
+            </div>
+            <div className="col-span-1 h-80">
+              <div className="relative h-full w-full">
+                <Image
+                  src="https://res.cloudinary.com/strcnigeria/image/upload/v1663237575/Mass%20Centers/Bode%20Mission/IMG-20220903-WA0017_uilijt.jpg"
+                  layout="fill"
+                  objectFit="cover"
+                  priority="true"
+                  alt="image"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-3 lg:col-span-1 order-1 lg:order-2">
+          <div className="mb-10 shadow-lg shadow-primary-light/30">
+            <h3 className="uppercase text-center text-white text-lg p-3 bg-primary-2">
+              address
+            </h3>
+            <div className="p-5">
+              <p>
+                Ugabi Street, Ewea Quarters, opposite Colonel Unokhua’s
+                compound.
+              </p>
+              <p>Agenebode, Etsako East, Edo State.</p>
+            </div>
+          </div>
+
+          <div className="mb-10 shadow-lg shadow-primary-light/30">
+            <h3 className="uppercase text-center text-white text-lg p-3 bg-primary-2">
+              priests
+            </h3>
+            <div className="p-5">
+              <p>Reverend Father Nkamuke Bede (+234 08165672764)</p>
+              <p>Reverend Father Ojeka Thomas (+234 07049047920)</p>
+              <p>Reverend Father Okerulu John (+234 07064809668)</p>
+            </div>
+          </div>
+
+          <div className="mb-10 shadow-lg shadow-primary-light/30">
+            <h3 className="uppercase text-center text-white text-lg p-3 bg-primary-2">
+              coordinators phone no
+            </h3>
+            <div className="p-5">
+              <p> +234 8165672764</p>
+              <p> +234 9132793973 </p>
+              <p> +234 8125064044</p>
+            </div>
+          </div>
+
+          <div className="mb-10 shadow-lg shadow-primary-light/30">
+            <h3 className="uppercase text-center text-white text-lg p-3 bg-primary-2">
+              directions
+            </h3>
+            <div className="p-5">
+              <h5 className="font-bold text-lg text-primary">
+                From Jattu junction Auchi (by motor):
+              </h5>
+              <p>
+                At the motor park, take a vehicle going to Agenebode (Bode). Ask
+                the driver to stop you in front of Microfinance Bank. Next,
+                cross the road. Then take the street (Ugabi Street) beside
+                Microfinance Bank. Keep walking on this street until you reach a
+                third building by your left. (The building is fenced with a red
+                roof and has a black gate).
+              </p>
+            </div>
+            <div className="px-5 pb-5">
+              <h5 className="font-bold text-lg text-primary leading-6">
+                From Upland Roundabout (by a motorcycle):
+              </h5>
+              <p>
+                At the Roundabout take a motorcycle and tell the bike man to
+                take you to Ugabi Street opposite Colonel Unokhua’s compound. It
+                is the fenced building directly opposite.
+              </p>
+            </div>
+          </div>
+
+          <div className="mb-10 shadow-lg shadow-primary-light/30">
+            <h3 className="uppercase text-center text-white text-lg p-3 bg-primary-2">
+              current activities
+            </h3>
+            <div className="p-5">
+              <p>
+                <span className="font-bold text-primary">Sunday:</span> Mass or
+                prayers in the absence of a Priest 10am.
+              </p>
+              <p>
+                <span className="font-bold text-primary">Rosary:</span> Daily.
+              </p>
+              <p>
+                <span className="font-bold text-primary">Friday:</span> Sacred
+                Heart Devotion 4pm
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Bode;
