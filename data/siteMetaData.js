6a7f80de26f931043252b7e7f0fb7e22f3363a7a
Peter Okerulu <oduchep@gmail.com>
2022-09-20 08:48:14 +0100

    changed active link style for sublinks and added SEO to pages

---
+++ data/siteMetaData.js
@@ -0,0 +1,15 @@
+const siteMetadata = {
+  companyName: 'STRC Nigeria',
+  phoneNumber: '+2349072065991',
+  siteUrl: 'strcnigeria.org',
+  siteLogo: '/favicon.ico',
+  siteLogoSquare: '/strc-nigeria logo_white.PNG',
+  email: 'FatherNkamuke@strcnigeria.org',
+  twitter: 'https://twitter.com/example.page',
+  twitterHandle: '@example',
+  facebook: 'https://m.facebook.com/sowseminary',
+  instagram: 'https://www.instagram.com/example.page',
+  linkedin: 'https://www.linkedin.com/in/example.page',
+};
+
+export default siteMetadata;
