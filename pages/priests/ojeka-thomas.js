0ab49b4cb51a4255c99a242b4832ba7134406853
Peter Okerulu <oduchep@gmail.com>
2022-10-12 07:44:22 +0100

    built father ojeka page

---
+++ pages/priests/ojeka-thomas.js
@@ -0,0 +1,151 @@
+import React from 'react';
+import HeadSeo from '../../components/headSEO';
+import siteMetadata from '../../data/siteMetaData';
+
+const OjekaThomas = () => {
+  return (
+    <>
+      <HeadSeo
+        title={`Ojeka Thomas`}
+        description={`the biography page of father ojeka thomas`}
+        canonicalUrl={siteMetadata.siteUrl}
+        ogTwitterImage={siteMetadata.siteLogoSquare}
+        ogType={'website'}
+      />
+
+      <div className="main-container container mx-auto">
+        <h1 className="capitalize text-center text-primary text-2xl md:text-3xl mb-8 font-medium">
+          About Fr. Ojeka
+        </h1>
+
+        <div>
+          <p className="text-lg mb-8">
+            Rev. Fr. Thomas T. Joseph Ayakana OJEKA was born on October 24th
+            1988 and received infant baptism about a month later; and received
+            Novus ordo <em>first unholy communion</em> in 2000, and
+            <em>non-Confirmation</em> in 2001. He was born and bred in Alifokpa
+            -Yache in Yala Local Government Area of Cross River State,
+            South-South geopolitical zone of Nigeria. He had one sister; born
+            when he was 14-going-15; but lost her a few months later. So
+            in-fact: he is an only child of subsistence farming parents.
+          </p>
+
+          <p className="text-lg mb-8">
+            He attended St. Francis primary school, Alifokpa, and did his Junior
+            Secondary education in Yache Community Secondary, Alifokpa. He had
+            his Senior Secondary education in Federal Science College, Ogoja,
+            where he graduated in 2006, but sat again for WASSCE 2007, in St.
+            Patrick&apos;s Technical College, Ugep, CRS.
+          </p>
+
+          <p className="text-lg mb-8">
+            In September of 2008, he was admitted as an Aspirant into the
+            &quot;Congregation of the Servants of Charity&quot;. As a temporary
+            professed member of this congregation, he earned a Bachelor Degree
+            in Philosophy [B. Ph.] from
+            <em>
+              {' '}
+              &quot;Don Bosco Institute of Philosophy, Ibadan, Nigeria&quot;{' '}
+            </em>
+            affiliated to the{' '}
+            <em> &quot;Salesian Pontifical University, Rome&quot;</em> in 2014.
+            During his first year as a student of theology [in the institution
+            called{' '}
+            <em>
+              SS. Peter and Paul Major Seminary, Bodija, Ibadan in Nigeria,
+              affiliated to Urban Pontifical University Rome
+            </em>
+            ], his lecturers sang the praises of the glories of Vatican II
+            too-much as it were, striking an odd note which left him wondering
+            about the immutability and indefectibility of the Church. He made,
+            what he calls the &quot;treasured mistake&quot; of looking things up
+            concerning the sung glories of Vatican II Council. This mistake led
+            him to discover modernism to be the spirit of Vatican II Council and
+            it&apos;s &apos;aggiornamento&apos; in doctrines, disciplines and
+            liturgy, making him to see in Sedevacantism the logical Catholic
+            response to the Vatican II question, which though may lead to
+            mystery, cannot lead to contradiction -using Fr. Cekada&apos;s
+            expression.
+          </p>
+
+          <p className="text-lg mb-8">
+            His journey to the priesthood began as a childhood dream! As a
+            minor, his dream was this:{' '}
+            <em>
+              &quot;I wish to become a [Catholic] priest if it is the will of
+              God&quot;
+            </em>
+            . It was as though [as he would later express it in a song] deep in
+            his heart he began to hear a voice so serene, still and tender,
+            clear and distinct beyond telling which kept saying{' '}
+            <em>&quot;whom shall I send? Who shall go...?&quot;</em> He seemed
+            to have answered{' '}
+            <em>
+              &quot;here am I Lord, I hear you call; yes I come to do your will,
+              but I cannot let you lead the way, take my hand gracious Lord,
+              lead me on. Take me, hold me, lead me on!&quot;
+            </em>{' '}
+            and as if the Lord said{' '}
+            <em>&apos;go you also into my vineyard...&apos;,</em> he understood
+            that to mean beginning the &apos;minor seminary&apos;. But due to
+            financial constraint he could not go to what was known to him as the
+            Diocesan minor seminary in Ogoja: it was as though his dream had
+            come to an end!
+          </p>
+
+          <p className="text-lg mb-8">
+            Coming of age, trusting that <em>&quot;God will provide</em>&quot;
+            he was resolute about making himself available for an opportunity
+            for the discernment of priestly vocation. He found in the Founder of
+            the <em>Congregation of the Servants of Charity</em>, a saintly
+            Italian priest, an Apostle of Charity, the{' '}
+            <strong>Servant of God</strong>, Don Luigi Guanella [who had
+            personal dealings with Pope St Pius X]; a priestly model. Ordained
+            priest on May 26, 1866 for the diocese of Como, Italy, his sole wish
+            was to be a{' '}
+            <em>
+              &apos;fiery sword in the ministry&apos;; &apos;to pray and to
+              suffer&apos;; &apos;to give bread and the Lord&apos;
+            </em>
+            : devoting all his gifts of grace and nature to the cause of the
+            Cross.
+          </p>
+
+          <p className="text-lg mb-8">
+            Being led sweetly by Providence, through the Mother Mother of Divine
+            Providence, to be healed of his previous total blindness concerning
+            Vatican II and its impostor hybrid-ecumenical-pan church/religion;
+            and, given to see clearly how his inspiring ideal is totally
+            compromised, he resolved to flee the modernist establishment,
+            content with being known as a lay faithful Catholic than to be a
+            lay-robed modernist puppet thinking himself a Catholic priest. And,
+            strengthened to put his resolve into action, fleeing he fled,
+            <em>totally</em>, with no scruples, on the 1st of August, 2017
+            -having completed nine years; even though the majority thought him,
+            [and they still do] deluded and a sore disappointment…
+          </p>
+
+          <p className="text-lg mb-8">
+            Providence ordering all things mercifully and sweetly, he began
+            (again) his seminary training under Fr. Nkamuke from September of
+            2017 as a student of <em>Sedes Sapientiae Seminary</em>, then in
+            Lagos. By Fr. Nkamuke&apos;s recommendation, and Bp. Dolan&apos;s
+            approval, he was admitted to Most Holy Trinity Seminary, then
+            Brooksville, FL, US in September of 2018; and eventually continued
+            again as student of Sedes Sapiantiae Seminary from June of 2021
+            while residing in Saint Getrude the Great Catholic Church, West
+            Chester, Ohio, USA. In sink with the provisions of the Canon Law and
+            the Liturgical Law of Holy Mother Church, he responded to the call
+            to ascend the Seven Steps unto the Altar of God beginning with the
+            reception of Clerical Tonsure on July 24th, 2019, the last step,
+            ordination to the priesthood, being on the 11th of May, 2022, in
+            Saint Getrude the Great Catholic Church, West Chester, Ohio, USA, by
+            His Excellency, Most Rev. Rodrigo Da Silva.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OjekaThomas;
