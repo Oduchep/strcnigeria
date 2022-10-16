12832414edb1b3685cc612e6709824c297874513
Peter Okerulu <oduchep@gmail.com>
2022-10-16 17:21:40 +0100

    updated site meta data file

--- data/siteMetaData.js
+++ data/siteMetaData.js
@@ -1,15 +1,19 @@
+import siteLogo from '../public/favicon.ico';
+import siteLogoSquare from '../public/strc-nigeria logo_white.PNG';
+
 const siteMetadata = {
   companyName: 'STRC Nigeria',
   phoneNumber: '+2349072065991',
   siteUrl: 'strcnigeria.org',
-  siteLogo: '/favicon.ico',
-  siteLogoSquare: '/strc-nigeria logo_white.PNG',
+  siteLogo: siteLogo,
+  siteLogoSquare: siteLogoSquare,
   email: 'FatherNkamuke@strcnigeria.org',
   twitter: 'https://twitter.com/example.page',
   twitterHandle: '@example',
   facebook: 'https://m.facebook.com/sowseminary',
   instagram: 'https://www.instagram.com/example.page',
   linkedin: 'https://www.linkedin.com/in/example.page',
+  youtube: 'https://youtube.com/channel/UCfWx0YMIK2gcPLE3uYbfmdA',
 };
 
 export default siteMetadata;
